1b9b4bab631d8745c274e344b159b251c3bce4ee
Mateus Kunrath <dev.mateuskunrath@gmail.com>
2025-06-06 22:34:50 -0300

    feat: add create user tests

---
+++ tests/users.spec.ts
@@ -0,0 +1,28 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+
+describe('Users routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'User test',
+      email: 'user@test.com',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
